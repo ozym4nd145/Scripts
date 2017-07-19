c2d2f710855d3efe6f4d71bbe6799f3773454f3b
ozym4nd145 <ozym4nd145@outlook.com>
2017-07-19 21:29:41 +0530

    Completed scraping academics1

--- Scrap_Acad1/get_courses.py
+++ Scrap_Acad1/get_courses.py
@@ -1,12 +1,12 @@
 import json
-with open("./course_db.json","r") as fl:
+import requests
+
+with open("./course_db.json", "r") as fl:
     courses = json.loads(fl.read())
-with open("./courses.txt","w") as fl:
-    for course in courses:
-        fl.write(course["code"].upper()+"\n")
 
 cookies = {
-    'SESS1fasdf2926bf876664ed5383994cb4c1de': 'asfdasdfsadfsfsadadfasdf',
+    '_ga': 'GA1.3.1716513787.1485603351',
+    'SESS1f002926bf876664ed5383994cb4c1de': 'XXXXXXXXXXXXXXXXXXXXXXXXXX',
 }
 
 headers = {
@@ -14,25 +14,56 @@ headers = {
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'en-US,en;q=0.8,hi;q=0.6',
     'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
     'Content-Type': 'application/x-www-form-urlencoded',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Cache-Control': 'max-age=0',
-    'Referer': 'https://academics1.iitd.ac.in/Academics/index.php?page=CourseCodeN&secret=sdafsdfasdfasdasdfasdfsadfasdfasdfsadf&uname=2015CS10262',
+    'Referer': 'https://academics1.iitd.ac.in/Academics/index.php?page=ListCourseN&secret=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX&uname=XXXXXXXXXXX',
     'Connection': 'keep-alive',
     'DNT': '1',
 }
 
-params = {
-    'page': 'ListCourseN',
-    'secret': 'asdfasdfsadsadfsadfsaddfsadfsadf',
-    'uname': '2015CS10262',
-}
+params = (
+    ('page', 'excel'),
+    ('secret', 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'),
+    ('uname', 'XXXXXXXXXXX'),
+)
 
 data = {
-    'EntryNumber': 'HSL212',
-    'submit-button': 'Submit',
+    'submit': 'Download Data in CSV File',
+    'EntryNumber': 'XXXXXX',
+    'UserID': 'XXXXXXXXXXX',
 }
 
-ans2 = requests.post('https://academics1.iitd.ac.in/Academics/index.php',
-              headers=headers, params=params, cookies=cookies, data=data, verify=False)
+students = {}
+for course in courses:
+    print("processing course: ",course["code"])
+    try:
+        data["EntryNumber"]=course["code"].upper()
+        response = requests.post('https://academics1.iitd.ac.in/Academics/GenerateExcel.php',
+                    headers=headers, params=params, cookies=cookies, data=data, verify=False)
+        if(response.status_code==200):
+            student_data = response.content.decode('utf-8').upper().splitlines()[4:]
+            student_data = [d.split(',') for d in student_data]
+            data_json = [{"name":d[2],"entry":d[1],"group":d[3],"audit_withdraw":d[4],"slot":d[5]} for d in student_data]
+            course["students"] = data_json
+            for student in course["students"]:
+                if student["entry"] not in students:
+                    students[student["entry"]]={"name":student["name"],"courses":[]}
+                main_course = course.copy()
+                del(main_course["students"])
+                student_data = student.copy()
+                del(student_data["name"])
+                del(student_data["entry"])
+                main_course.update(student_data)
+                students[student["entry"]]["courses"].append(main_course)
+    except Exception as e:
+        print("Error occurred in course ",course["code"])
+        print(e)
+
+with open("student_data.json","w") as fl:
+    fl.write(json.dumps(students,indent=4,sort_keys=True))
+with open("course_data.json","w") as fl:
+    fl.write(json.dumps(courses,indent=4,sort_keys=True))
+
+
