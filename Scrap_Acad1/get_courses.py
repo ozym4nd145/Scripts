40f223f595ef5053ceaf2305e5d13814090767b9
ozym4nd145 <ozym4nd145@outlook.com>
2017-07-20 15:23:14 +0530

    Modified script for better structure

--- Scrap_Acad1/get_courses.py
+++ Scrap_Acad1/get_courses.py
@@ -1,8 +1,9 @@
 import json
 import requests
+import os
 
 with open("./course_db.json", "r") as fl:
-    courses = json.loads(fl.read())
+    course_list = json.loads(fl.read())
 
 cookies = {
     '_ga': 'GA1.3.1716513787.1485603351',
@@ -35,22 +36,44 @@ data = {
     'UserID': 'XXXXXXXXXXX',
 }
 
+CSV_DIR = "./csv_files"
+os.makedirs("./csv_files", exist_ok=True)
+
 students = {}
-for course in courses:
-    print("processing course: ",course["code"])
+courses = {}
+for course in course_list:
+    code = course["code"].upper()
+    slot = course["slot"].upper()
+    if (code not in courses):
+        courses[code] = {}
+    courses[code][slot] = course
+    courses[code][slot]["students"] = []
+
+course_codes = list(set([x["code"].upper() for x in course_list]))
+course_codes.sort()
+
+for idx,code in enumerate(course_codes):
+    print("processing course: %s , %d of %d" %(code,idx+1,len(course_codes)))
     try:
-        data["EntryNumber"]=course["code"].upper()
+        data["EntryNumber"] = code
         response = requests.post('https://academics1.iitd.ac.in/Academics/GenerateExcel.php',
-                    headers=headers, params=params, cookies=cookies, data=data, verify=False)
-        if(response.status_code==200):
-            student_data = response.content.decode('utf-8').upper().splitlines()[4:]
+                                 headers=headers, params=params, cookies=cookies, data=data, verify=False)
+
+        if(response.status_code == 200):
+            csv_content = response.content.decode('utf-8').upper()
+            with open(os.path.join(CSV_DIR, code + ".csv"), "w") as fl:
+                fl.write(csv_content)
+            student_data = csv_content.splitlines()[4:]
             student_data = [d.split(',') for d in student_data]
-            data_json = [{"name":d[2],"entry":d[1],"group":d[3],"audit_withdraw":d[4],"slot":d[5]} for d in student_data]
-            course["students"] = data_json
-            for student in course["students"]:
+            data_json = [{"name": d[2].upper(), "entry":d[1].upper(), "group":d[3].upper(),
+                         "audit_withdraw":d[4].upper(), "slot":d[5].upper()} for d in student_data]
+            for student in data_json:
+                courses[code][student["slot"]]["students"].append(student)
+            for student in data_json:
                 if student["entry"] not in students:
-                    students[student["entry"]]={"name":student["name"],"courses":[]}
-                main_course = course.copy()
+                    students[student["entry"]] = {
+                        "name": student["name"], "courses": []}
+                main_course = courses[code][student["slot"]].copy()
                 del(main_course["students"])
                 student_data = student.copy()
                 del(student_data["name"])
@@ -58,12 +81,10 @@ for course in courses:
                 main_course.update(student_data)
                 students[student["entry"]]["courses"].append(main_course)
     except Exception as e:
-        print("Error occurred in course ",course["code"])
+        print("Error occurred in course ", course["code"])
         print(e)
 
-with open("student_data.json","w") as fl:
-    fl.write(json.dumps(students,indent=4,sort_keys=True))
-with open("course_data.json","w") as fl:
-    fl.write(json.dumps(courses,indent=4,sort_keys=True))
-
-
+with open("student_data.json", "w") as fl:
+    fl.write(json.dumps(students, indent=4, sort_keys=True))
+with open("course_data.json", "w") as fl:
+    fl.write(json.dumps(courses, indent=4, sort_keys=True))
