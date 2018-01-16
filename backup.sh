c662845af64a9a669d0b076a59ed6624530b0b73
ozym4nd145 <ozym4nd145@outlook.com>
2018-01-17 00:43:38 +0530

    Updated backup script

--- backup.sh
+++ backup.sh
@@ -1,16 +1,20 @@
 #! /bin/bash
-rsync -r -t -o -v --progress --modify-window=1 -s /home/suyash1212/Coding /media/suyash1212/Suyash_1TB/Backup
-rsync -r -t -o -v --progress --modify-window=1 -s /home/suyash1212/bin /media/suyash1212/Suyash_1TB/Backup
-rsync -r -t -o -v --progress --modify-window=1 -s /home/suyash1212/Screenshots /media/suyash1212/Suyash_1TB/Backup
-rsync -r -t -o -v --progress --modify-window=1 -s /home/suyash1212/.config /media/suyash1212/Suyash_1TB/Backup/dotfiles
-rsync -r -t -o -v --progress --modify-window=1 -s /home/suyash1212/.i3 -c -s /media/suyash1212/Suyash_1TB/Backup/dotfiles
-rsync -t -v -o -h --progress --modify-window=1 -s /home/suyash1212/.bashrc -c -s /media/suyash1212/Suyash_1TB/Backup/dotfiles
-rsync -t -v -o -h --progress --modify-window=1 -s /home/suyash1212/.bash_history -c -s /media/suyash1212/Suyash_1TB/Backup/dotfiles
-rsync -t -v -o -h --progress --modify-window=1 -s /home/suyash1212/.dircolors -c -s /media/suyash1212/Suyash_1TB/Backup/dotfiles
-rsync -t -v -o -h --progress --modify-window=1 -s /home/suyash1212/.vimrc -c -s /media/suyash1212/Suyash_1TB/Backup/dotfiles
-rsync -t -v -o -h --progress --modify-window=1 -s /home/suyash1212/.vim -c -s /media/suyash1212/Suyash_1TB/Backup/dotfiles
-rsync -t -v -o -h --progress --modify-window=1 -s /home/suyash1212/.tmux.conf -c -s /media/suyash1212/Suyash_1TB/Backup/dotfiles
-rsync -t -v -o -h --progress --modify-window=1 -s /home/suyash1212/.gitconfig -c -s /media/suyash1212/Suyash_1TB/Backup/dotfiles
-rsync -t -v -o -h --progress --modify-window=1 -s /home/suyash1212/installed_programs.dat -c -s /media/suyash1212/Suyash_1TB/Backup/dotfiles
-rsync -t -v -o -h --progress --modify-window=1 -s /home/suyash1212/systemlog.txt -c -s /media/suyash1212/Suyash_1TB/Backup/dotfiles
-rsync -r -t -o -v --progress --modify-window=1 -s /media/500_GB/Important /media/suyash1212/Suyash_1TB/Backup
+rsync -r -t -o -v --progress --modify-window=1 -s /home/suyash1212/Coding /media/500_GB/Backup
+rsync -r -t -o -v --progress --modify-window=1 -s /home/suyash1212/bin /media/500_GB/Backup
+rsync -r -t -o -v --progress --modify-window=1 -s /home/suyash1212/Screenshots /media/500_GB/Backup
+rsync -r -t -o -v --progress --modify-window=1 --max-size='2MB' --exclude-from='/home/suyash1212/bin/exclude_list.txt' -s /home/suyash1212/.config /media/500_GB/Backup/dotfiles
+rsync -r -t -o -v --progress --modify-window=1 -s /home/suyash1212/.i3 -c -s /media/500_GB/Backup/dotfiles
+rsync -t -v -o -h --progress --modify-window=1 -s /home/suyash1212/.bashrc -c -s /media/500_GB/Backup/dotfiles
+rsync -t -v -o -h --progress --modify-window=1 -s /home/suyash1212/.bash_history -c -s /media/500_GB/Backup/dotfiles
+rsync -t -v -o -h --progress --modify-window=1 -s /home/suyash1212/.dircolors -c -s /media/500_GB/Backup/dotfiles
+rsync -t -v -o -h --progress --modify-window=1 -s /home/suyash1212/.vimrc -c -s /media/500_GB/Backup/dotfiles
+rsync -t -v -o -h --progress --modify-window=1 -s /home/suyash1212/.vim -c -s /media/500_GB/Backup/dotfiles
+rsync -t -v -o -h --progress --modify-window=1 -s /home/suyash1212/.tmux.conf -c -s /media/500_GB/Backup/dotfiles
+rsync -t -v -o -h --progress --modify-window=1 -s /home/suyash1212/.gitconfig -c -s /media/500_GB/Backup/dotfiles
+rsync -t -v -o -h --progress --modify-window=1 -s /home/suyash1212/installed_programs.dat -c -s /media/500_GB/Backup/dotfiles
+rsync -t -v -o -h --progress --modify-window=1 -s /home/suyash1212/systemlog.txt -c -s /media/500_GB/Backup/dotfiles
+rsync -r -t -o -v --progress --modify-window=1 -s /media/500_GB/Important /media/500_GB/Backup
+
+rsync -r -t -o -v --progress --modify-window=1 -s /boot/grub/grub.cfg -c -s /media/500_GB/Backup/sys_config/ --delete
+
+echo "Backup on "$(date) >> ~/logs/backup.log
