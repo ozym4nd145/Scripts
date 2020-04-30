f462c2b4e445a12fe88c74f937baafaf381b4891
Suyash Agrawal <ozym4nd145@outlook.com>
2020-05-01 06:16:49 +0800

    Add andromeda server backup

---
+++ andromeda_backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -o nounset # no undeclared variable use
+set -e # fail on non zero return
+
+DEST="/data/andromeda"
+COMMAND="rsync -avz"
+
+declare -a dotPaths=(".bashrc" ".vimrc" ".tmux.conf" ".config" ".gitconfig" ".dircolors" ".fzf" ".fzf.bash" ".gnupg" ".bash_eternal_history" ".bash_history" ".profile" ".ssh" ".vim")
+declare -a homePaths=("bin" "man" "letsencrypt")
+declare -a otherPaths=()
+
+for p in "${dotPaths[@]}"; do
+  eval ${COMMAND} ${HOME}/${p} ${DEST}/dotFiles/
+done
+for p in "${homePaths[@]}"; do
+  eval ${COMMAND} ${HOME}/${p} ${DEST}/homeFiles/
+done
+for p in "${otherPaths[@]}"; do
+  eval ${COMMAND} ${p} ${DEST}/otherFiles/
+done
