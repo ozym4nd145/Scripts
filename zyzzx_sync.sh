b0645ff3e22aefcfd740ebf26cb619879ed28d97
Suyash Agrawal <ozym4nd145@outlook.com>
2020-08-02 21:13:27 +0800

    Change sync for nas

--- zyzzx_sync.sh
+++ zyzzx_sync.sh
@@ -6,12 +6,12 @@ echo "-----------------------------------------------------"
 echo "$(date)"
 echo "-----------------------------------------------------"
 
-DEST="andromeda:/data/zyzzx"
-COMMAND="rsync -rluptzzvP --delete --exclude '__pycache__' --exclude '*.pyc' --exclude '.git' --exclude '*.o' --exclude 'node_modules' --ignore-missing-args --max-size='100M' --filter=':- .gitignore'"
+DEST="hippocampus:/volume1/data/zyzzx/backup"
+COMMAND="rsync -rluptzvP --delete --exclude '__pycache__' --exclude '*.pyc' --exclude '.git' --exclude '*.o' --exclude 'node_modules' --ignore-missing-args --max-size='100M' --filter=':- .gitignore'"
 
 crontab -l > "${HOME}"/.crontab.bak
-declare -a dotPaths=(".bashrc" ".vimrc" ".tmux.conf" ".config" ".gitconfig" ".dircolors" ".dmenurc" ".dmrc" ".fzf" ".i3" ".urxvt" ".wpmrc" ".wpm.csv" ".Xauthority" ".xinitrc" ".Xresources" ".gnupg" ".bash_eternal_history" ".bash_history" ".profile" ".ssh" ".vim" ".crontab.bak")
-declare -a homePaths=("bin" "man" "colorschemes" "notes.txt" "Tower")
+declare -a dotPaths=(".bashrc" ".vimrc" ".tmux.conf" ".config" ".gitconfig" ".dircolors" ".dmenurc" ".dmrc" ".fzf" ".i3" ".urxvt" ".wpmrc" ".wpm.csv" ".Xauthority" ".xinitrc" ".Xresources" ".gnupg" ".bash_eternal_history" ".bash_history" ".profile" ".ssh" ".vim" ".crontab.bak" ".emacs.d" ".doom.d")
+declare -a homePaths=("bin" "man" "colorschemes" "notes.txt" "Tower" "org" "secret")
 declare -a otherPaths=("/data")
 
 fullDotPaths=("${dotPaths[@]/#/${HOME}/}")
